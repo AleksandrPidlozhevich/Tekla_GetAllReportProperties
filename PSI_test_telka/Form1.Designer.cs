6fe1c03aa58214b8647dbe7082f886aaa7f41f17
AleksandrPidlozhevich <78640105+AleksandrPidlozhevich@users.noreply.github.com>
2021-09-01 20:07:45 +0300

    started work

---
+++ PSI_test_telka/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace PSI_test_telka
+{
+    partial class ReportPropertiesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.elem_tanlang = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl_ID = new System.Windows.Forms.Label();
+            this.lbl_connection = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.elem_tanlang);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.lbl_ID);
+            this.groupBox1.Controls.Add(this.lbl_connection);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(426, 396);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Свойства";
+            // 
+            // elem_tanlang
+            // 
+            this.elem_tanlang.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.elem_tanlang.Location = new System.Drawing.Point(6, 71);
+            this.elem_tanlang.Name = "elem_tanlang";
+            this.elem_tanlang.Size = new System.Drawing.Size(197, 33);
+            this.elem_tanlang.TabIndex = 6;
+            this.elem_tanlang.Text = "Выбрать элемент";
+            this.elem_tanlang.UseVisualStyleBackColor = true;
+            this.elem_tanlang.Click += new System.EventHandler(this.elem_tanlang_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(6, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Наличие Соединения";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl_ID
+            // 
+            this.lbl_ID.AutoSize = true;
+            this.lbl_ID.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_ID.Location = new System.Drawing.Point(305, 77);
+            this.lbl_ID.Name = "lbl_ID";
+            this.lbl_ID.Size = new System.Drawing.Size(29, 21);
+            this.lbl_ID.TabIndex = 5;
+            this.lbl_ID.Text = "ID";
+            // 
+            // lbl_connection
+            // 
+            this.lbl_connection.AutoSize = true;
+            this.lbl_connection.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_connection.Location = new System.Drawing.Point(305, 38);
+            this.lbl_connection.Name = "lbl_connection";
+            this.lbl_connection.Size = new System.Drawing.Size(65, 21);
+            this.lbl_connection.TabIndex = 5;
+            this.lbl_connection.Text = "Да/Нет";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 25);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Вывод.";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // ReportPropertiesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(426, 396);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "ReportPropertiesForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Информация квадрат(колонна)";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbl_connection;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button elem_tanlang;
+        private System.Windows.Forms.Label lbl_ID;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ PSI_test_telka/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Windows.Forms;
+using Tekla.Structures.Model.UI;
+using Tekla.Structures.Model;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace PSI_test_telka
+{
+    public partial class ReportPropertiesForm : Form
+    {
+        private Part _part;
+        Model model;
+        public ReportPropertiesForm()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Model _model = new Model();
+            if (_model.GetConnectionStatus())
+            {
+                model = _model;
+                lbl_connection.Text = "Есть соединени";
+            }
+            else
+            {
+                lbl_connection.Text = "Нет соединени";
+            }
+        }
+        private void elem_tanlang_Click(object sender, EventArgs e)
+        {
+            ArrayList sNames = new ArrayList();
+            sNames.Add("NAME");
+            sNames.Add("SCREW_NAME");
+            sNames.Add("SCREW_TYPE");
+            sNames.Add("TYPE");
+            sNames.Add("TYPE1");
+            sNames.Add("TYPE2");
+            sNames.Add("TYPE3");
+            sNames.Add("TYPE4");
+            sNames.Add("STANDARD");
+            sNames.Add("SHORT_NAME");
+            sNames.Add("MATERIAL");
+            sNames.Add("FINISH");
+            sNames.Add("GRADE");
+            ArrayList iNames = new ArrayList();
+            iNames.Add("DATE");
+            iNames.Add("FATHER_ID");
+            iNames.Add("GROUP_ID");
+            iNames.Add("HIERARCHY_LEVEL");
+            iNames.Add("MODEL_TOTAL");
+            ArrayList dNames = new ArrayList();
+            dNames.Add("EXTRA_LENGTH");
+            dNames.Add("FLANGE_THICKNESS");
+            dNames.Add("FLANGE_WIDTH");
+            dNames.Add("HEIGHT");
+            dNames.Add("LENGTH");
+            dNames.Add("PRIMARYWEIGHT");
+            dNames.Add("PROFILE_WEIGHT");
+            dNames.Add("ROUNDING_RADIUS");
+            dNames.Add("LENGTH");
+            dNames.Add("DIAMETER");
+            dNames.Add("WEIGHT");
+            dNames.Add("HEAD_DIAMETER");
+            dNames.Add("THICKNESS");
+            dNames.Add("WASHER.THICKNESS");
+            dNames.Add("WASHER.INNER_DIAMETER");
+            dNames.Add("WASHER.OUTER_DIAMETER");
+            dNames.Add("WASHER.THICKNESS1");
+            dNames.Add("WASHER.INNER_DIAMETER1");
+            dNames.Add("WASHER.OUTER_DIAMETER1");
+            dNames.Add("WASHER.THICKNESS2");
+            dNames.Add("WASHER.INNER_DIAMETER2");
+            dNames.Add("WASHER.OUTER_DIAMETER2");
+            dNames.Add("NUT.THICKNESS");
+            dNames.Add("NUT.INNER_DIAMETER");
+            dNames.Add("NUT.OUTER_DIAMETER");
+            dNames.Add("NUT.THICKNESS2");
+            dNames.Add("NUT.OUTER_DIAMETER2");
+            try
+            {
+                var picker = new Picker();
+                _part = picker.PickObject(Picker.PickObjectEnum.PICK_ONE_PART) as Part;
+                lbl_ID.Text = _part.Identifier.ID.ToString();
+                Hashtable sValues = new Hashtable(sNames.Count + dNames.Count + iNames.Count);
+                if (_part.GetAllReportProperties(sNames, dNames, iNames, ref sValues))
+                {
+                    string a = "";
+                    foreach (DictionaryEntry value in sValues)
+                    {
+                        a += $"{value.Key}: {value.Value}\n";
+                    }
+                    label5.Text = a;
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
